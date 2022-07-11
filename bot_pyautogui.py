746bf0ecdcd24575456a203fb60ff0320462f2ee
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-11 14:45:48 -0300

    Create bot_pyautogui.py

---
+++ bot_pyautogui.py
@@ -0,0 +1,21 @@
+import pyautogui as pa
+import time
+
+# abrir a ferramenta/ o sistema/ o programa
+# pa.PAUSE = 2
+# pa.press("win")
+# pa.write("login.xlsx")
+# pa.press("backspace")
+# pa.press("enter")
+
+# descobrir a posição do login
+time.sleep(3)
+pa.position() # mostra a posição do mouse
+
+# preencher o login
+
+
+# preencher a senha
+
+
+# clicar em fazer login
