f087acc539b20e0d1e0057133616cb94f5baa265
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-12 13:03:57 -0300

    esperando carregar o login

--- bot_selenium.py
+++ bot_selenium.py
@@ -9,17 +9,18 @@ webdrive = webdriver.Chrome(executable_path=chromedrive_path) # variavel que ama
 sleep(2)
 
 webdrive.get("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1657565493&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d1ef1bb96-2b2b-f0c4-a64b-21f24fec1b8e&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015") # link da página que queremos entrar
-sleep(10)
+while len(webdrive.find_elements(By.NAME, 'psRNGCDefaultType')) < 1:
+    sleep(1)
 
 login = webdrive.find_element(By.ID, 'i0116') # procurar pelo ID para preencher o login
-login.send_keys("SeuEmail@outlook.com") # escreve no login
-sleep(5)
+login.send_keys("sergio.lima53@etec.sp.gov.br") # escreve no login
+sleep(2)
 
 btn_proximo = webdrive.find_element(By.ID, 'idSIButton9').click() # procura pelo ID o botão, e clica nele
 sleep(15)
 
 senha = webdrive.find_element(By.ID, 'i0118') # procurar pelo ID para preencher a senha
-senha.send_keys("SuaSenha") # escreve na senha
+senha.send_keys("papagaio12@") # escreve na senha
 sleep(5)
 
 btn_entrar = webdrive.find_element(By.ID, 'idSIButton9').click() # procura pelo ID o botão, e clica nele 
